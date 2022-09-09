ba73b5a60611cf4706a75590462a4b411d0b0d76
j0achim <joachim.nordvik@gmail.com>
2022-09-09 13:21:23 +0200

    Update mock.lua
    cleanup

--- src/server/GOAP/mock.lua
+++ src/server/GOAP/mock.lua
@@ -1,32 +1,32 @@
-local types = require("goap")
+local goap = require("goap")
 
 local goals = {
-    keepFeed = types.Goal.new("keep feed", {hunger = true}, 0, nil),
-    wander = types.Goal.new("wander", {wandering = false}, 0, nil),
-    idle = types.Goal.new("idle", {idle = false}, 10, nil),
-    killEnemy = types.Goal.new("kill player", {enemyDead = true}, 0, nil),
-    syatWarm = types.Goal.new("stay warm", {cold = true}, 0, nil),
+    goap.Goal.new("keep feed", {hunger = true}, 0, nil),
+    goap.Goal.new("wander", {wandering = false}, 0, nil),
+    goap.Goal.new("idle", {idle = false}, 10, nil),
+    goap.Goal.new("kill player", {enemyDead = true}, 0, nil),
+    goap.Goal.new("stay warm", {cold = true}, 0, nil),
 }
 
 local actions = {
-    tinder = types.Action.new("gather tinder and wood", 1000, nil, {haveTinder = true, haveWood = true}),
-    buildFire = types.Action.new("build camp fire", 1000, {haveWood = true}, {haveCampFire = true, haveWood = false}),
-    keepFireAlive = types.Action.new("keep fire alive", 0, {haveTinder = true, haveCampFire = true}, { haveTinder = false, cold = true}),
-    walk = types.Action.new("walk", 0, nil, {wandering = false}),
-    idle = types.Action.new("do nothing", 0, nil, {idle = false}),
-    chaseEnemy = types.Action.new("chase", 20, nil, {enemyInRange = true}),
-    stunEnemy = types.Action.new("stun", 5, nil, {enemyDead = true}),
-    rangedAttack = types.Action.new("ranged attack", 0, nil, {enemyDead = true}),
-    meleeAttack = types.Action.new("melee attack", 0, {enemyInRange = true}, {enemyDead = true}),
-    getMeat = types.Action.new("gather Meat", 1600, nil, {haveMeat = true}),
-    killBoar = types.Action.new("kill Boar", 1000, nil, {haveMeat = true}),
-    eat = types.Action.new("eat meat", 10, {haveMeat = true}, {haveMeat = false, hunger = true}),
-    gatherWood = types.Action.new("gather wood", 1000, nil, {haveWood = true}),
-    gatherGems = types.Action.new("gather gems", 1000, nil, {haveGems = true}),
-    tradeGems = types.Action.new("trade gems for meat", 50, {haveGems = true}, {haveMeat = true, haveGems = false}),
-    tradeWood = types.Action.new("trade wood for gold", 100, {haveWood = true}, {haveGold = true, haveWood = false}),
-    tradeMeat = types.Action.new("trade gold for meat", 200, {haveGold = true}, {haveMeat = true, haveGold = false}),
-    tradegold = types.Action.new("trade gold for gems", 200, {haveGold = true}, {haveGems = true, haveGold = false}),
+    goap.Action.new("gather tinder and wood", 1000, nil, {haveTinder = true, haveWood = true}),
+    goap.Action.new("build camp fire", 1000, {haveWood = true}, {haveCampFire = true, haveWood = false}),
+    goap.Action.new("keep fire alive", 0, {haveTinder = true, haveCampFire = true}, { haveTinder = false, cold = true}),
+    goap.Action.new("walk", 0, nil, {wandering = false}),
+    goap.Action.new("do nothing", 0, nil, {idle = false}),
+    goap.Action.new("chase", 20, nil, {enemyInRange = true}),
+    goap.Action.new("stun", 5, nil, {enemyDead = true}),
+    goap.Action.new("ranged attack", 0, nil, {enemyDead = true}),
+    goap.Action.new("melee attack", 0, {enemyInRange = true}, {enemyDead = true}),
+    goap.Action.new("gather Meat", 1600, nil, {haveMeat = true}),
+    goap.Action.new("kill Boar", 1000, nil, {haveMeat = true}),
+    goap.Action.new("eat meat", 10, {haveMeat = true}, {haveMeat = false, hunger = true}),
+    goap.Action.new("gather wood", 1000, nil, {haveWood = true}),
+    goap.Action.new("gather gems", 1000, nil, {haveGems = true}),
+    goap.Action.new("trade gems for meat", 50, {haveGems = true}, {haveMeat = true, haveGems = false}),
+    goap.Action.new("trade wood for gold", 100, {haveWood = true}, {haveGold = true, haveWood = false}),
+    goap.Action.new("trade gold for meat", 200, {haveGold = true}, {haveMeat = true, haveGold = false}),
+    goap.Action.new("trade gold for gems", 200, {haveGold = true}, {haveGems = true, haveGold = false}),
 }
 
 local function Reverse(list: table): table
@@ -42,7 +42,7 @@ local function Timed(func) : number | any
     return (os.clock() - startTime) * 1000, result
 end
 
-local goap = types.Goap.new()
+local goap = goap.Goap.new()
 
 local loadTime = Timed(function() return goap:DirectLoad(goals, actions) end)
 
